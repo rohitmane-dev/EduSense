6c76f0e805b43113f48d751e69a26ce11abf6951
R Mane <engineeringduration@gmail.com>
2025-12-26 04:12:29 +0530

    changes made in dobt section

--- client/src/store/useDoubtStore.js
+++ client/src/store/useDoubtStore.js
@@ -4,10 +4,28 @@ const useDoubtStore = create((set) => ({
     doubts: [],
     loading: false,
     error: null,
+    askingQuestion: false,
 
     setDoubts: (doubts) => set({ doubts }),
-    addDoubt: (doubt) => set((state) => ({ doubts: [doubt, ...state.doubts] })),
+
+    addDoubt: (doubt) => set((state) => ({
+        doubts: [doubt, ...state.doubts]
+    })),
+
+    updateDoubt: (id, updates) => set((state) => ({
+        doubts: state.doubts.map((d) =>
+            (d.doubtId === id || d._id === id ? { ...d, ...updates } : d)
+        )
+    })),
+
+    removeDoubt: (id) => set((state) => ({
+        doubts: state.doubts.filter((d) => d.doubtId !== id && d._id !== id)
+    })),
+
+    setAskingQuestion: (askingQuestion) => set({ askingQuestion }),
+
     setLoading: (loading) => set({ loading }),
+
     setError: (error) => set({ error }),
 }));
 
